f11cf7dd9e63716568e8bf08349e1bbb4731ea5a
Dadi B <benmezianedadi@yahoo.fr>
2024-01-20 20:39:08 +0100

    correction de l'apparition des bullets du carrousel

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -1,30 +1,58 @@
 import { useEffect, useState } from "react";
+
+
 import { useData } from "../../contexts/DataContext";
 import { getMonth } from "../../helpers/Date";
 
+
 import "./style.scss";
+
 
 const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ?-1 : 0
+    new Date(evtA.date) < new Date(evtB.date) ? -1 : 0
   );
-  const nextCard = () => {
-    setTimeout(
-      () => setIndex(index < byDateDesc.length-1 ? index + 1 : 0),
-      5000
+
+
+ // Trie le tableau focus contenu dans l'objet data par date dans l'ordre décroissant. Du plus ancien au plus récent.
+  // Méthode sort avec une fonction de comparaison basée sur les dates
+  // -1 indique que si l'evenetment A est plus ancien que B, il doit etre placé avant le B (evenement le plus récent)
+
+
+ const nextCard = () => {
+    setIndex((prevIndex) =>
+      prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0
     );
+
+
+   // NextCard : utilise la fonction setIndex pour mettre à jour l'index.
+    // L'index est incrémenté de 1 s'il est inférieur à la longueur du tableau trié,
+    // sinon il est réinitialisé à 0.
   };
-  useEffect(() => {
-    nextCard();
-  });
-  return (
+
+
+ useEffect(
+    () => {
+      const intervalId = setInterval(nextCard, 5000);
+
+
+     return () => clearInterval(intervalId);
+    },
+    // eslint-disable-next-line
+    [index, byDateDesc]
+    // useEffect : exécute la fonction nextCard toutes les 5 secondes
+    // clearInterval : arrête l'exécution de la fonction nextCard
+    // La dépendance [index, byDateDesc] indique que l'effet doit être réexécuté lorsque l'une de ces valeurs change.
+  );
+
+
+ return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <>
+        <div key={event.title}>
           <div
-            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
@@ -42,18 +70,20 @@ const Slider = () => {
             <div className="SlideCard__pagination">
               {byDateDesc.map((_, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={{radioIdx}}
                   type="radio"
+                  readOnly
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
           </div>
-        </>
+        </div>
       ))}
     </div>
   );
 };
+
 
 export default Slider;
